4b9651eede5f4e3c013793854a2e8952d12116ec
mary-ue <maryandreeva100@gmail.com>
2023-12-08 00:05:33 +0300

    add index.html's styles and fonts

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiperThumb = new Swiper('.gift__swiper--thumb', {
+  spaceBetween: 16,
+  slidesPerView: 6,
+  freeMode: true,
+});
+
+const swiperMain = new Swiper('.gift__swiper--card', {
+  spaceBetween: 16,
+  thumbs: {
+    swiper: swiperThumb,
+  },
+});
