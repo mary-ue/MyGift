3e813209ad216f0f6c0e22e35b4a2f89dfcc3d11
mary-ue <maryandreeva100@gmail.com>
2023-12-09 00:45:51 +0300

    adaptives and card.html's styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const swiperThumb = new Swiper('.gift__swiper--thumb', {
-  spaceBetween: 16,
-  slidesPerView: 6,
+  spaceBetween: 12,
+  slidesPerView: 'auto', // swiper не будет рассчитывать width слайдов
   freeMode: true,
+  breakpoints: {
+    320: {
+      spaceBetween: 12,
+    },
+    1141: {
+      spaceBetween: 16,
+    }
+  }
 });
 
 const swiperMain = new Swiper('.gift__swiper--card', {
