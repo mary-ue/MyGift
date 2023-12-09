f4280d6ad6d675c423df2118a75f5c0ba04b7335
mary-ue <maryandreeva100@gmail.com>
2023-12-10 00:01:49 +0300

    rerrange card, mask and validate form

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ const swiperThumb = new Swiper('.gift__swiper--thumb', {
     },
     1141: {
       spaceBetween: 16,
-    }
-  }
+    },
+  },
 });
 
 const swiperMain = new Swiper('.gift__swiper--card', {
@@ -17,4 +17,67 @@ const swiperMain = new Swiper('.gift__swiper--card', {
   thumbs: {
     swiper: swiperThumb,
   },
+});
+
+// imask
+const phoneInputs = document.querySelectorAll('.form__field--phone');
+
+for (let i = 0; i < phoneInputs.length; i++) {
+  const element = phoneInputs[i];
+  IMask(element,
+    {
+      mask: '+{7}(000)000-00-00',
+    });
+}
+
+// validate 
+const form = document.querySelector('.form');
+const submitButton = form.querySelector('.form__button');
+
+const updateSubmitButton = () => {
+  let isFormFilled = true;
+
+  for (const field of form.elements) {
+    if (field.classList.contains('form__field')) {
+      if (!field.value.trim()) {
+        isFormFilled = false;
+        break;
+      }
+    }
+  }
+
+  submitButton.disabled = !isFormFilled;
+}
+
+const phoneValidateOption = {
+  presence: {
+    message: 'Поле телефон обязательно для заполнения',
+  }, 
+  format: {
+    pattern: '\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}',
+    message: 'Номер телефона должен соответствовать формату: +7(XXX)XXX-XX-XX',
+  }
+}
+
+form.addEventListener('input', updateSubmitButton);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const errors = validate(form, {
+    sender_phone: phoneValidateOption,
+    receiver_phone: phoneValidateOption,
+  });
+
+  if (errors) {
+    for (const key in errors) {
+      const errorString = errors[key];
+      alert(errorString);
+    }
+    return;
+  }
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
 });
